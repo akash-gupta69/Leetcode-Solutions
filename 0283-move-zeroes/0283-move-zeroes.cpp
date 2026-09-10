d5d73e6f855dfb15febffd3bf562672232387752
Akash gupta Atmakuri <150373991+akash-gupta69@users.noreply.github.com>
2026-09-10 22:07:57 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int slow = 0;
-        for (int fast=0; fast<nums.size(); fast++){
-            if(nums[fast] != 0){
-                swap(nums[slow],nums[fast]);
-                slow++;
-            }
-        }  
+    void moveZeroes(vector<int>& nums){
+    // int i = 0;
+    int j = 0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i] != 0){
+            swap(nums[i],nums[j]);
+            j++;
+        }
+       } 
     }
 };
